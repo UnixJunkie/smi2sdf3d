07c949f9682fca5f52feada1b42d03d619fbb6f8
Francois BERENGER <ligand@free.fr>
2017-06-14 11:01:30 +0900

    corrected all typos

--- smi2sdf.py
+++ smi2sdf.py
@@ -9,11 +9,23 @@ if len (sys.argv) != 4:
     print "usage: %s N input.smi output.sdf" % sys.argv[0]
     sys.exit(1)
 
-n_confs = int(sys.argv[1])
-input_smi = sys.argv[2]
-output_sdf = sys.argv[3]
+# n_confs = int(sys.argv[1])
+# input_smi = sys.argv[2]
+# output_sdf = sys.argv[3]
+n_confs = 10
+input_smi = "ethanol.smi"
+output_sdf = "ethanol.sdf"
 
-reader = Chem.SmilesMolSupplier(input_smi)
+def RobustSmilesMolSupplier(filename):
+    with open(filename) as f:
+        for line in f:
+            split = line.split()
+            smile = split[0]
+            # mol_name = split[1]
+            mol = Chem.MolFromSmiles(smile)
+            yield mol
+
+reader = RobustSmilesMolSupplier(input_smi)
 writer = Chem.SDWriter(output_sdf)
 
 def how_many_conformers(mol):
@@ -34,21 +46,21 @@ def rmsd_filter(mol, ref_conf, l):
     refConfId = ref_conf.GetId()
     for e, curr_conf in l:
         currConfId = curr_conf.GetId()
-        if AllChem.GetBestRMS(mol, mol, refConfId, currConfIf) > rmsd_threshold:
+        if AllChem.GetBestRMS(mol, mol, refConfId, currConfId) > rmsd_threshold:
             res.append((e, curr_conf))
     return res
 
 for mol in reader:
     if mol:
         n = how_many_conformers(mol)
-        mol_H = Chem.addHs(mol)
+        print "n: %d" % n
+        mol_H = Chem.AddHs(mol)
         # FBR: check doc for EmbedMultipleConfs
         confIds = AllChem.EmbedMultipleConfs(mol_H, n)
         conf_energies = []
         # FF minimization
-        for conf in confIds:
-            confId = conf.get_Id()
-            ff = AllChem.UFFGetMoleculeForceField(mol_h, conf_id=confId)
+        for confId in confIds:
+            ff = AllChem.UFFGetMoleculeForceField(mol_H, confId=confId)
             ff.Minimize()
             energy = ff.CalcEnergy()
             conformer = mol_H.GetConformer(confId)
@@ -62,15 +74,15 @@ for mol in reader:
         while nb_out < n_confs and len(conf_energies) > 0:
             ++nb_out
             (e, conf) = conf_energies.pop(0)
-            keep.append((e, conf))
+            kept.append((e, conf))
             # remove neighbors
-            conf_energies = rmsd_filter(mol_h, conf, conf_energies)
+            conf_energies = rmsd_filter(mol_H, conf, conf_energies)
         # write them out
-        res = Chem.Mol(mol_h)
+        res = Chem.Mol(mol_H)
         res.RemoveAllConformers()
         for e, conf in kept:
             confId = conf.GetId()
-            conf = mol_h.GetConformer(confId)
+            conf = mol_H.GetConformer(confId)
             res.AddConformer(conf, assignId=True)
         writer.write(res)
 writer.close()
