8ef07d807f1443a7d6c523767a7ff791e55aaa80
Francois BERENGER <ligand@free.fr>
2017-06-14 16:14:43 +0900

    added license header

--- smi2sdf.py
+++ smi2sdf.py
@@ -6,6 +6,25 @@
 # "Freely Available Conformer Generation Methods: How Good Are They?"
 # JCIM, 2012, DOI: 10.1021/ci2004658 for technical details
 
+# Copyright (C) 2017 Francois BERENGER
+# System Cohort Division,
+# Medical Institute of Bioregulation,
+# Kyushu University
+# 3-1-1 Maidashi, Higashi-ku, Fukuoka 812-8582, Japan
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import rdkit
 from rdkit import Chem
