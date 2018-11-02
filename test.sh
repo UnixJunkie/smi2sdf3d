7a4429c11a86a773291f92146faad65f19dea781
Francois Berenger <unixjunkie@sdf.org>
2018-11-02 15:46:40 +0900

    better test

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-\rm -f data/test_output.sdf
+out=data/test_output.sdf
 
-./smi2sdf.py -n 10 -i data/test_input.smi -o data/test_output.sdf
+\rm -f ${out}
+
+./smi2sdf.py -n 10 -i data/test_input.smi -o ${out}
+
+echo '#molecules in output:'
+which molcount 2>&1 >/dev/null && molcount ${out}
